a9c45d13b31884fc2903b32b0767c8fb6f42b5d8
Zed Hanok <16245029+hanok2@users.noreply.github.com>
2022-09-07 10:56:01 -0400

    Lighting up the Field of Vision (FOV)
    Lighting up the Field of Vision (FOV) around the player--helps a ton!

--- engine.py
+++ engine.py
@@ -10,7 +10,7 @@ from tcod.console import Console
 from entity import Entity
 from game_map import GameMap
 from input_handler import EventHandler
-
+from tcod.map import compute_fov
 
 class Engine:
     def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
@@ -18,6 +18,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -28,13 +29,30 @@ class Engine:
             
             action.perform(self, self.player)
 
+            self.update_fov() # update the Field of Vision next action
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
         console.clear()
     
+
+
+##
--- tile_types.py
+++ tile_types.py
@@ -6,7 +6,7 @@ from typing import Tuple
 
 import numpy as np  # type: ignore
 
-# Tile graphics structured type compatible with Console.tiles_rgb.
+# Tile graphics structured compatible with Console.tiles_rgb.
 graphic_dt = np.dtype(
     [
         ("ch", np.int32),  # Unicode codepoint.
@@ -21,6 +21,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool),  # True if this tile can be walked over.
         ("transparent", np.bool),  # True if this tile doesn't block FOV.
         ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt),  # Graphics for when the tile is in FOV.
     ]
 )
 
@@ -30,15 +31,24 @@ def new_tile(
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    """Helper function for defining individual tile types """
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    """A helper function for defining individual tile types """
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
     #walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (24, 0, 0)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord(" "), (255, 255, 255), (24, 0, 0)),
+    light=(ord(" "), (255, 255, 255), (48, 12, 4)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (72, 0, 0)),
+    walkable=False, 
+    transparent=False, 
+    dark=(ord(" "), (255, 255, 255), (72, 0, 0)),
+    light=(ord(" "), (255, 255, 255), (144, 0, 0)),
 )
